c5123e589901bc910c5fe2ec9c0e7c42e6a7138c
WilliamTsou818 <cccc1589@gmail.com>
2021-06-27 20:46:11 +0800

    feat: add login and register route and page

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
@@ -12,9 +12,7 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
