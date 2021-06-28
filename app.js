856798a0d7c77368c56bf4b6a50be9697d8f06ee
WilliamTsou818 <cccc1589@gmail.com>
2021-06-28 16:43:53 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes')
 
 const app = express()
@@ -10,6 +11,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'MyTodo',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 
