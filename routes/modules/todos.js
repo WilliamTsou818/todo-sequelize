4e2057985e7808823fed31273e1f4aedc0929224
WilliamTsou818 <cccc1589@gmail.com>
2021-06-28 17:29:20 +0800

    feat: add new page and route to create todo

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,6 +3,18 @@ const db = require('../../models')
 const Todo = db.Todo
 const router = express.Router()
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const userId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name: name, UserId: userId })
+    .then(() => res.redirect('/'))
+    .catch(err => console.error(err))
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
